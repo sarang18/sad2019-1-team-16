0b7dffbeeed0a82b1e6662e9ec7861039cbde867
DESKTOP-HGPF4DU\Sanket <sanketwarambhe@gmail.com>
2019-01-18 17:30:04 +0100

    Adding bootstrap classes to the application

--- Blog Engine/app.js
+++ Blog Engine/app.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+//Strategy for storing images
+const storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    //rejecting a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+} 
+const upload = multer({storage: storage,
+    limit: {
+        fileSize: 1024 * 1024 * 5 //max 5 mb image upload
+    },
+    fileFilter: fileFilter
+});
 
 mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true });
 let db = mongoose.connection;
@@ -21,7 +49,7 @@ const app = express();
 let Article = require('./models/articles');
 
 //Loading view engines
-//app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Middleware for body parser
@@ -33,7 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res, next) => {
-    article.find({}, function(err, articles) {
+    Article.find({}, function(err, articles) {
         if(err){
             console.log(err);
         }
